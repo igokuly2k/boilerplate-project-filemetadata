517059648c1728b939d485527f51e50ebe971d9b
Gokul E <igokulsmailbox@gmail.com>
2022-01-27 23:34:14 +0530

    Project Working

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+const upload = multer();
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +13,11 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=> {
+  res.json({name:req.file.originalname,
+  type:req.file.mimetype,
+  size:req.file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
